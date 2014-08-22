7339a9a6f9693ace4eb941a4333aedcae4fd5832
Dave Burke <thoughtcriminall@gmail.com>
2014-08-22 11:52:44 -0500

    Added comments to explain rdiff-backup switches

--- system-backup.sh
+++ system-backup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+START=$(date +%s)
+
+# $(uname -n) resolves to the hostname
 DEST=servy::/mnt/media/backup/systems/$(uname -n)
 
-START=$(date +%s)
+# /dev, /proc, /sys, /tmp, /run, /var/cache, and /var/run, and /lost+found are dynamically created system directories that should not be backed up.
+# /mnt and /media are excluded to avoid backing up external media (USB drives) and remote file shares.
+# /.snapshots are btrfs snapshots.
+# --preserve-numerical-ids allows the backup to move between systems with different usernames and still be restored.
+# --verbosity 5 prints the names of files that changed.
 rdiff-backup \
 	--exclude='/dev/*' \
 	--exclude='/proc/*' \
@@ -13,15 +20,17 @@ rdiff-backup \
 	--exclude='/media/*' \
 	--exclude='/lost+found' \
 	--exclude='/var/cache/*' \
+	--exclude='/var/run/*' \
 	--exclude='/.snapshots/*' \
 	--preserve-numerical-ids \
 	--verbosity 5 \
 	/ $DEST 
-FINISH=$(date +%s)
-echo "Backup completed in $(( ($FINISH-$START) / 60 )) minutes, $(( ($FINISH-$START) % 60 )) seconds"
 
 echo "Verifying..."
 rdiff-backup --verify $DEST
 
 echo "Cleaning up increments older than one month..."
 rdiff-backup --remove-older-than 1M $DEST
+
+FINISH=$(date +%s)
+echo "Backup completed in $(( ($FINISH-$START) / 60 )) minutes, $(( ($FINISH-$START) % 60 )) seconds"
